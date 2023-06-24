6c9c04a5bbd680affbcb16beb3cc1e9661628079
Poshya-hub <talasuposhya2003@gmail.com>
2023-06-24 03:51:15 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,105 @@
+import "./App.css";
+import { useEffect, useState, useRef } from "react";
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import Message from "./Message";
+import db from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@mui/icons-material/Send";
+import IconButton from "@mui/material/IconButton";
+import logo1 from "./assests/logo1.png";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    setUsername(prompt("Enter the user Name"));
+
+    db.collection("messenger")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs
+            .map((doc) => ({ id: doc.id, message: doc.data() }))
+            .reverse()
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    db.collection("messenger").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
+  return (
+    <div className="App">
+      <div className="sub_head">
+        <div className="logo_image">
+          <img src={logo1} alt="IMGAE" className="ac_img" />
+          <h3>Welcome {username}</h3>
+        </div>
+      </div>
+      <h3 className="greet">
+        IT'S POSHYA, LET'S make a chat experience that's both posh and
+        delightful
+      </h3>
+      <div className="message-container">
+        <FlipMove>
+          {messages.map(({ id, message }) => {
+            return (
+              <Message
+                key={id}
+                username={username}
+                message={message}
+                timestamp={message.timestamp}
+              />
+            );
+          })}
+        </FlipMove>
+        <div ref={messagesEndRef} />
+      </div>
+
+      <form className="app_form" onSubmit={sendMessage}>
+        <FormControl className="app_formControl">
+          <InputLabel>Enter a message</InputLabel>
+          <Input
+            className="app_input"
+            placeholder="Enter a Message.... "
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <IconButton
+            className="app_iconButton"
+            type="submit"
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            text="submit"
+            onClick={sendMessage}
+          >
+            <SendIcon />
+          </IconButton>
+        </FormControl>
+      </form>
+    </div>
+  );
+}
+
+export default App;
